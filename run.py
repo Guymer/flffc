f7da26db0a7d229cae7f52adf63b1e478f4a8eee
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2018-05-19 13:11:07 +0100

    increased resolution of background image

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def run(dirOut = "FLFFCoutput", country = "United Kingdom", steps = 50):
                 lat_max + 0.1
             ]
         )
-        pyguymer.add_map_background(ax, resolution = "medium2048px")
+        pyguymer.add_map_background(ax, resolution = "large4096px")
         ax.coastlines(
             resolution = "10m",
             color = "black",
