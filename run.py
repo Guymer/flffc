04f1590ba3821436937c41276399a637b44734e0
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-08-17 17:34:38 +0100

    use new wrapper function

--- run.py
+++ run.py
@@ -147,6 +147,5 @@ def run(dirOut = "FLFFCoutput", country = "United Kingdom", steps = 50):
                     dpi = 300,
              pad_inches = 0.1
         )
-        pyguymer3.exiftool(os.path.join(dirOut, "{:s}.png".format(country)))
-        pyguymer3.optipng(os.path.join(dirOut, "{:s}.png".format(country)))
+        pyguymer3.optimize_image(os.path.join(dirOut, "{:s}.png".format(country)), strip = True)
         matplotlib.pyplot.close("all")
