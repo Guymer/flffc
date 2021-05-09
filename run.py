98ece83eb42853ab672aaf0644c87f485851f71d
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-05-09 13:13:28 +0100

    standardize comment

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def run(dirOut = "FLFFCoutput", country = "United Kingdom", steps = 50):
         raise Exception("\"cartopy\" is not installed; run \"pip install --user Cartopy\"") from None
     try:
         import matplotlib
-        matplotlib.use("Agg")                                                   # NOTE: https://matplotlib.org/gallery/user_interfaces/canvasagg.html
+        matplotlib.use("Agg")                                                   # NOTE: See https://matplotlib.org/stable/gallery/user_interfaces/canvasagg.html
         import matplotlib.pyplot
     except:
         raise Exception("\"matplotlib\" is not installed; run \"pip install --user matplotlib\"") from None
