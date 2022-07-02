dc1c3e46fc16a1107dce4d18741b65d1c03708e5
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-02 12:46:31 +0100

    replace os.path.join with f-strings

--- __init__.py
+++ __init__.py
@@ -6,12 +6,5 @@ location that is the furthest away from the coast in a particular country. It
 also contains a wrapper function to perform the job for you.
 """
 
-# Import standard modules ...
-import sys
-
 # Import sub-functions ...
 from .run import run
-
-# Ensure that this module is only imported by Python 3.x ...
-if sys.version_info.major != 3:
-    raise Exception("the Python module \"flffc\" must only be used with Python 3.x") from None
