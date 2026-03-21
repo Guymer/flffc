9105c98d12ce80688eaadd829bae969ee62ac415
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-03-21 09:47:14 +0000

    add a BASH script to run the Python scripts

---
+++ toRun.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Check that non-standard programs are installed. "standard" programs are
+# anything that is specified in the POSIX.1-2008 standard (and the IEEE Std
+# 1003.1 standard) or that is a BASH builtin command. Therefore, "non-standard"
+# programs are anything that does not appear on the following two lists:
+#   * https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
+#   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+
+# Create short-hands ...
+fillFactor=0.02
+nAng=181
+ramLimit=4294967296
+simpFactor=0.0004
+
+# Run Python script ...
+python3.12 newMethodScope.py                                                    \
+    --fill-factor "${fillFactor}"                                               \
+    --nAng "${nAng}"                                                            \
+    --RAM-limit ${ramLimit}
+
+# Loop over GSHHG resolutions ...
+for gshhgRes in "c" "l" "i" "h" "f"; do
+    # Run Python script ...
+    python3.12 newMethod.py                                                     \
+        --fill-factor "${fillFactor}"                                           \
+        --GSHHG-resolution "${gshhgRes}"                                        \
+        --nAng "${nAng}"                                                        \
+        --RAM-limit "${ramLimit}"                                               \
+        --simplification-factor "${simpFactor}"
+done
+
+# Run Python script ...
+python3.12 plotNewMethod.py                                                     \
+    --fill-factor "${fillFactor}"                                               \
+    --nAng "${nAng}"                                                            \
+    --RAM-limit "${ramLimit}"                                                   \
+    --simplification-factor "${simpFactor}"                                     \
+    --timeout 600.0
