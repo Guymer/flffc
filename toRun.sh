9a858a4656eee1522d30195b2005840e87683b5f
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-03-22 10:44:09 +0000

    check for Python; move timeout to the top

--- toRun.sh
+++ toRun.sh
@@ -6,12 +6,17 @@
 # programs are anything that does not appear on the following two lists:
 #   * https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
 #   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+if ! type python3.12 &> /dev/null; then
+    echo "ERROR: \"python3.12\" is not installed." >&2
+    exit 1
+fi
 
 # Create short-hands ...
 fillFactor=0.02
 nAng=181
 ramLimit=4294967296
 simpFactor=0.0004
+timeout=600.0
 
 # Run Python script ...
 python3.12 newMethodScope.py                                                    \
@@ -36,4 +41,4 @@ python3.12 plotNewMethod.py                                                     
     --nAng "${nAng}"                                                            \
     --RAM-limit "${ramLimit}"                                                   \
     --simplification-factor "${simpFactor}"                                     \
-    --timeout 600.0
+    --timeout "${timeout}"
